f28c5609a3aa1876819fee829735fdbfab5cc157
J <74514216+thecloudybot@users.noreply.github.com>
2021-03-08 17:19:14 -0500

    Update hw.lua

--- u/hw.lua
+++ u/hw.lua
@@ -1,2 +1,7 @@
-127994407 | beta_millions | hwid = '4B6F7-46132-34323-73833-31363-43' |
-300029997 | beta_witz | hwid = ' ' |
+127994407 | beta_millions | hwid = L(5)
+300029997 | beta_witz | hwid = L(7)
+
+
+4B6F7-46132-34323-73833-31363-43
+
+--witz
