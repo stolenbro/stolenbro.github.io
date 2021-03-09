98006a84d95b2abf95bd5c804a2c761089d48cb2
J <74514216+thecloudybot@users.noreply.github.com>
2021-03-08 22:28:24 -0500

    Update hw.lua

--- u/hw.lua
+++ u/hw.lua
@@ -1,5 +1,10 @@
-127994407 | beta_millions | hwid = L(5)
-300029997 | beta_witz | hwid = L(7)
+127994407  |  beta_millions | hwid = L(6)
+300029997  |  beta_witz | hwid = L(8)
+1451185589 |  beta_5rsm | hwid = L(10)
 
 
-'4B6F7-46132-34323-73833-31363-43'
+'4B6F7-46132-34323-73833-31363-43' -- millions
+
+' ' -- witz
+
+' ' -- 5rsm
